f01c37a064cc17e345b829d73ace2d80a4905e9f
Muntyboss <bossmunty@gmail.com>
2023-02-28 09:04:09 -0800

    8 puts

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -11,8 +12,10 @@ void print_array(int *a, int n)
 	int i;
 
 	for (i = 0; i < (n - 1); i++)
-		printf("%d, ", a[i]);
-		if (i == (n - 1))
-			printf("%d", a[n - 1]);
+	{
+		printf("%d", a[i]);
+		if (i != (n - 1))
+			printf(", ");
+	}
 	printf("\n");
 }
