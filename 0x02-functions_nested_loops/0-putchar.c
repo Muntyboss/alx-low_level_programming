bfbd72ce2d67f4e731ad5fc8dccdb592319958aa
Muntyboss <bossmunty@gmail.com>
2023-02-28 16:44:07 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 
-int main(void) {
-    putchar('_');
-    putchar('p');
-    putchar('u');
-    putchar('t');
-    putchar('c');
-    putchar('h');
-    putchar('a');
-    putchar('r');
-    putchar('\n');
-    return 0;
+/**
+ * include stdio.h
+ *int main
+ * a program that prints _putchar, followed by a new line.
+ *
+ * Retrun: void
+ */
+
+int main(void)
+{
+	 putchar('_');
+	 putchar('p');
+	 putchar('u');
+	 putchar('t');
+	 putchar('c');
+	 putchar('h');
+	 putchar('a');
+	 putchar('r');
+	 putchar('\n');
+
+return (0);
 }
 
